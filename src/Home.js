dc2c775fad8bdbe46cdeb192d761edb35ff4dd84
Bilal Sherwani <bilalsherwanirbc@gmail.com>
2021-03-30 00:40:26 +0500

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from './components/Header/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer/Footer'
+import Logo from './components/Header/logo'
+import Carousel from './components/carousel'
+import About from './components/homeabout'
+import ProductLine from './components/productline'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Box = styled.div`
+background: rgb(0,212,255);
+background: linear-gradient(156deg, rgba(0,212,255,1) 0%, rgba(2,159,220,1) 10%, rgba(4,118,193,1) 27%, rgba(5,87,172,1) 42%, rgba(8,38,140,1) 58%, rgba(24,9,121,1) 83%);
+height: 100%;
+width: 100%;
+
+
+
+`
+
+function Screen() {
+    return (
+      
+        <Box>
+              
+            <Navbar>
+
+            </Navbar>
+                
+                    <Logo>
+                        
+                    </Logo>
+                        <Carousel>
+
+                        </Carousel>
+                        <About>
+                            
+                        </About>
+                        <ProductLine>
+                            
+                        </ProductLine>
+                    
+                    <Footer>
+
+                    </Footer>
+                 
+        </Box>
+    );
+
+}
+
+export default Screen;

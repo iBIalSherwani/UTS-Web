1a5904c0cc5abee5f65b6926475b068f45ef7ee8
Bilal Sherwani <bilalsherwanirbc@gmail.com>
2021-03-30 00:38:58 +0500

    Add files via upload

---
+++ src/AboutUs.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from './components/Header/navbar'
+import Footer from './components/Footer/Footer'
+import Logo from './components/Header/logo'
+import AboutUs from './components/about'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Box = styled.div`
+background: rgb(0,212,255);
+background: linear-gradient(156deg, rgba(0,212,255,1) 0%, rgba(2,159,220,1) 10%, rgba(4,118,193,1) 27%, rgba(5,87,172,1) 42%, rgba(8,38,140,1) 58%, rgba(24,9,121,1) 83%);
+height: 100%;
+width: 100%;
+
+`
+
+function aboutUs() {
+    return (
+        <Box>
+            <Navbar>
+
+            </Navbar>
+                
+                    <Logo>
+                        
+                    </Logo>
+                    <AboutUs/>
+                   
+                    <Footer>
+
+                    </Footer>
+                 
+        </Box>
+    );
+
+}
+
+export default aboutUs;
